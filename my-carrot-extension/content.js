73099a340ed1649114445d68982cf2a025de4dcb
amineAUPEC <79785242+amineAUPEC@users.noreply.github.com>
2025-01-28 21:46:37 +0100

    Update content.js carrot warning

--- my-carrot-extension/content.js
+++ my-carrot-extension/content.js
@@ -1,3 +1,9 @@
+// const images = {
+//     "gitlab": "assets/carrot_green.svg",
+//     "jira": "assets/carrot_red.svg",
+//     "confluence": "assets/carrot_blue.svg",
+//     "efrei": "assets/carrot_green.svg"
+// };
 const images = {
     "gitlab": "https://raw.githubusercontent.com/amineAUPEC/replace-carrot-img-extension/refs/heads/main/my-carrot-extension/assets/carrot_green.svg",
     "jira": "https://raw.githubusercontent.com/amineAUPEC/replace-carrot-img-extension/refs/heads/main/my-carrot-extension/assets/carrot_red.svg",
@@ -11,9 +17,19 @@ function replaceImages() {
     let imageSrc;
 
     if (url.includes("gitlab.com")) {
+        alert("gitlab be careful carrot !");
         imageSrc = images.gitlab;
-        image=document.querySelector("#be-navigation-mobile > div.be-nav-tablet > a > svg");
-        image = imageSrc;
+        setTimeout(() => {
+            image=document.querySelector("#be-navigation-mobile > div.be-nav-tablet > a > svg");
+            const svgElement = document.querySelector("#be-navigation-mobile > div.be-nav-tablet > a > svg");
+            // image = imageSrc;
+            if (svgElement) {
+                const imgElement = document.createElement("img");
+                imgElement.src = imageSrc;
+                svgElement.parentNode.replaceChild(imgElement, svgElement);
+            }
+        }, 10000);
+  
 
     } else if (url.includes("efrei.fr")) {
         imageSrc = images.efrei;
